7ae7a779de72a428cd38d6e86d9f539cb3c093bb
Shintaro Jokagi <taroj1205@gmail.com>
2025-10-10 12:01:45 +1300

    chore: try adding not found components

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -1,14 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/404")({
-  component: App,
+	component: App,
 });
 
 function App() {
-  return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-6xl font-bold mb-4">404</h1>
-      <p className="text-xl">Page Not Found</p>
-    </div>
-  );
+	return (
+		<div className="flex flex-col items-center justify-center h-full">
+			<h1 className="text-6xl font-bold mb-4">404</h1>
+			<p className="text-xl">Page Not Found</p>
+			<p className="text-xs">(404.tsx)</p>
+		</div>
+	);
 }
